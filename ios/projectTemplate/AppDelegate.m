d178f8aba9f6be2921f0ea4374856245dbf3aea8
Roshan Maddumage <uomroshan@gmail.com>
2019-08-29 15:19:04 +0530

    google sign in added to ios platform

--- ios/projectTemplate/AppDelegate.m
+++ ios/projectTemplate/AppDelegate.m
@@ -6,10 +6,11 @@
  */
 
 #import "AppDelegate.h"
-
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <RNGoogleSignin/RNGoogleSignin.h>
+#import <Firebase.h>
 
 @implementation AppDelegate
 
@@ -27,6 +28,7 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [FIRApp configure];
   return YES;
 }
 
@@ -39,4 +41,11 @@
 #endif
 }
 
+
+- (BOOL)application:(UIApplication *)application openURL:(nonnull NSURL *)url options:(nonnull NSDictionary<NSString *,id> *)options {
+  return [RNGoogleSignin application:application
+                             openURL:url
+                   sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]
+                          annotation:options[UIApplicationOpenURLOptionsAnnotationKey]];
+}
 @end
